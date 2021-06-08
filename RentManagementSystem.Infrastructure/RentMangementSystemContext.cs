dc4f876e56e8a8c1292f0bdd6d2f87a72d750d2a
saiflearner <passionleadstosucces@gmail.com>
2021-06-08 14:37:17 +0500

    Context class and its properties added

--- RentManagementSystem.Infrastructure/RentMangementSystemContext.cs
+++ RentManagementSystem.Infrastructure/RentMangementSystemContext.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using RentManagementSystem.Data;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace RentManagementSystem.Infrastructure
 {
     public class RentMangementSystemContext : DbContext
     {
+        public RentMangementSystemContext() : base("name = RentManagement")
+        {
+           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<RentMangementSystemContext, //Migrations.Configuration>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Property> Property { get; set; }
+        public DbSet<Flat> Flats { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Rent> Rent { get; set; }
+        public DbSet<Bill> Bill { get; set; }
+
     }
 }
